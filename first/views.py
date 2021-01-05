47304c572b3aadc9543512d2c090c32cb86641b4
leear <pooa8@naver.com>
2021-01-05 22:23:57 +0900

    Feat : Add select, result page - 화면 출력 테스트를 위한 페이지 추가

--- first/views.py
+++ first/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.template import loader
+from datetime import datetime
+
+
+def index(request):
+    now = datetime.now()
+    context = {
+        'current_date': now
+    }
+    return render(request, 'index.html', context)
+
+
+def select(request):
+    context = {'number':4}
+    return render(request, 'select.html', context)
+
+
+def result(request):
+    context = {'number':[1,2,3,4,5,6]}
+    return render(request, 'result.html', context)
--- firstdjango/urls.py
+++ firstdjango/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from first import views
 
 urlpatterns = [
+    path('', include('first.urls')),
     path('admin/', admin.site.urls),
 ]
