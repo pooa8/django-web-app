a2b9ed0959331b168487852dd793924906e4f807
leear <pooa8@naver.com>
2021-01-07 22:10:43 +0900

    Chore : Delete secret information

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ld2s@43+(zcqbzn81h!9_gt22m%*e3a!&heuvg4oz!ede^obwg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
